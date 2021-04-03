c2b66245545bbaf416f35a1a1d9f950c8ca4ed61
Fenris Belserion <ichbinfenris@Gmail.com>
2021-04-03 14:51:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React, { useState } from 'react';
+import { LineLogin } from 'reactjs-line-login';
+import 'reactjs-line-login/dist/index.css';
 
 function App() {
+  const [payload, setPayload] = useState(null);
+  const [idToken, setIdToken] = useState(null);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LineLogin
+        clientID={process.env.REACT_APP_CLIENT_ID}
+        clientSecret={process.env.REACT_APP_CLIENT_SECRET}
+        state={process.env.REACT_APP_STATE}
+        nonce={process.env.REACT_APP_NONCE}
+        redirectURI={process.env.REACT_APP_REDIRECT_URI}
+        scope={process.env.REACT_APP_SCOPE}
+        setPayload={setPayload}
+        setIdToken={setIdToken}
+      />
+      {payload && idToken ? (
+        console.log('payload', payload, 'idToken', idToken)
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
